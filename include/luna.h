7e887722719e66071b88d160179620b4e1242cca
Ixaruz <44379110+Ixaruz@users.noreply.github.com>
2021-03-19 18:54:57 +0100

    Update luna.h

--- include/luna.h
+++ include/luna.h
@@ -4,20 +4,21 @@
 
 #define TID									0x01006F8002326000
 
-//*1.8.0 specific stuff*//
+//*1.9.0 specific stuff*//
 
 #define BID									0x0E36C71C08334076
-#define MAINFILE_SIZE						8690736 //same as 1.7.0
+#define MAINFILE_SIZE						8836448
 
-#define REVISION_MAJOR						0x78001
-#define REVISION_MINOR						0x78001
-#define REVISION_SAVE						17
+
+#define REVISION_MAJOR						0x7C001
+#define REVISION_MINOR						0x7C006
+#define REVISION_SAVE						18
 
 //**********************//
 
 #define MAJOR_VERSION						0
 #define MINOR_VERSION						2
-#define REVISION_VERSION					0
+#define REVISION_VERSION					1
 #define STRINGIFY(x)						#x
 #define TOSTRING(x)							STRINGIFY(x)
 #define STRING_VERSION						"" TOSTRING(MAJOR_VERSION) "." TOSTRING(MINOR_VERSION) "." TOSTRING(REVISION_VERSION) ""
