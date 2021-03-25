df8035113fad8cc62ff53a4b46cf9e41f316e48f
Ixaruz <44379110+Ixaruz@users.noreply.github.com>
2021-03-25 19:21:13 +0100

    Update util.cpp

--- source/util.cpp
+++ source/util.cpp
@@ -24,7 +24,6 @@ extern const u64 houseLvlOffset = 0x308bd4; //changed in 1.9.0
 extern const u64 EventFlagOffset = 0x22d9a8; //changed in 1.9.0
 
 static const char verboten[] = { ',', '/', '\\', '<', '>', ':', '"', '|', '?', '*', '™', '©', '®' };
-
 static bool isVerboten(const u16& t)
 {
     for (unsigned i = 0; i < 13; i++)
